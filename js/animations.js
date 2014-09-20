ea77dd43c22cee419090808fa925a52d7d752652
Trever Rohn <trohn12@gmail.com>
2014-09-19 22:50:15 -0500

    Added fade to flash

--- js/animations.js
+++ js/animations.js
@@ -1,7 +1,19 @@
 $(document).ready(bindEvents);
 
 function bindEvents() {
-	$("@flash").click();
-	$("@slide").click();
-	$("@color").click();
+	$("#flash").click(flashTrever);
+	$("#slide").click(flashTrever);
+	$("#color").click(flashTrever);
+}
+
+function flashTrever() {
+	 $("#flash").fadeToggle(500);
+}
+
+function slideTrever() {
+	
+}
+
+function colorTrever() {
+	
 }
